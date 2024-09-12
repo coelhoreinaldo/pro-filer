dc3a1cb8deb6afde78722c1a4e26bfe86b0b7ee5
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-12 18:24:23 -0300

    fix: show_deepest_file logic

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,7 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=lambda x: "/" in x)
+        deepest_file = max(context["all_files"], key=lambda x: x.count("/"))
         print(f"Deepest file: {deepest_file}")
 
 
